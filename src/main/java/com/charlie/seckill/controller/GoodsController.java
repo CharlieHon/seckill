b6bc2f1f2b162432b8ba664a900047fcb1face6c
charlie <2295509496@qq.com>
2024-09-07 16:27:58 +0800

    显示商品列表、根据id显示商品详细信息

--- src/main/java/com/charlie/seckill/controller/GoodsController.java
+++ src/main/java/com/charlie/seckill/controller/GoodsController.java
@@ -1,11 +1,14 @@
 package com.charlie.seckill.controller;
 
 import com.charlie.seckill.pojo.User;
+import com.charlie.seckill.service.GoodsService;
 import com.charlie.seckill.service.UserService;
+import com.charlie.seckill.vo.GoodsVo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
@@ -20,6 +23,9 @@ public class GoodsController {
 
     @Resource
     private UserService userService;
+
+    @Resource
+    private GoodsService goodsService;
 
     //// 进入商品列表页-将user对象保存到session中，session存入redis
     //@RequestMapping("/toList")
@@ -72,7 +78,29 @@ public class GoodsController {
             return "login";
         }
 
+        // 将user用户信息放入到model，携带给下一个模板使用
         model.addAttribute("user", user);
+        // 将商品列表信息，放入到
+        model.addAttribute("goodsList", goodsService.findGoodsVo());
         return "goodsList";
+    }
+
+    /**
+     * 方法：进去到商品详情页面
+     * @param user 通过自定义参数解析器处理返回的
+     * @param goodsId 用户点击详情时，携带过来的
+     */
+    @RequestMapping("/toDetail/{goodsId}")
+    public String toDetail(Model model, User user, @PathVariable("goodsId") Long goodsId) {
+        if (null == user) { // 说明用户没有登录
+            return "login";
+        }
+        model.addAttribute("user", user);
+
+        // 通过goodsId，获取秒杀商品信息
+        GoodsVo goodsVo = goodsService.findGoodsVoByGoodsId(goodsId);
+        // 将查询到的goodsVo放入到model，携带给下一个模板使用
+        model.addAttribute("goods", goodsVo);
+        return "goodsDetail";
     }
 }
