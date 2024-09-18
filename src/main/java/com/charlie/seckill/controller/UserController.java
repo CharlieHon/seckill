ac0d4742b3f92b37a24bc559927dd0e9abb87fd3
charlie <2295509496@qq.com>
2024-09-18 22:07:40 +0800

    对象缓存：修改对象信息后，从redis缓存中删除该对象

--- src/main/java/com/charlie/seckill/controller/UserController.java
+++ src/main/java/com/charlie/seckill/controller/UserController.java
@@ -1,14 +1,22 @@
 package com.charlie.seckill.controller;
 
 import com.charlie.seckill.pojo.User;
+import com.charlie.seckill.service.UserService;
 import com.charlie.seckill.vo.RespBean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    @Resource
+    private UserService userService;
 
     // 方法：返回登录用户的信息，同时接收请求参数address
     @RequestMapping("/info")
@@ -17,4 +25,10 @@ public class UserController {
         return RespBean.success(user);
     }
 
+    // 处理更新密码请求
+    @RequestMapping("/updpwd")
+    @ResponseBody
+    public RespBean updatePassword(String userTicket, String password, HttpServletRequest req, HttpServletResponse resp) {
+        return userService.updatePassword(userTicket, password, req, resp);
+    }
 }
