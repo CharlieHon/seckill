f9ee665705e0fa5cdf1e856d515b0c7d91fcad26
charlie <2295509496@qq.com>
2024-09-20 15:58:42 +0800

    消息发送者/生产者

---
+++ src/main/java/com/charlie/seckill/rabbitmq/MQSender.java
@@ -0,0 +1,27 @@
+package com.charlie.seckill.rabbitmq;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * MQSender：消息的发送者/生产者
+ */
+@Service
+@Slf4j
+public class MQSender {
+
+    // 装配RabbitTemplate->操作RabbitMQ
+    @Resource
+    private RabbitTemplate rabbitTemplate;
+
+    // 方法：发送消息
+    public void send(Object msg) {
+      log.info("发送消息-->" + msg);
+      // "queue"队列名
+      rabbitTemplate.convertAndSend("queue", msg);
+    }
+
+}
